01bd0efa57f8f3d16ae179d45580ada5a0ed9608
magus0 <harvestashya@gmail.com>
2022-06-01 02:38:53 -0700

    move building filter option dict into helper

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from helpers import build_filter_option_from_form
 from providers import Provider_list
 from constants import PROVIDER_JSON, FILTER_OPTIONS, STR_TRAITS, TRAITS, SEXES
-from providers_types import Filter_Options
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
@@ -35,35 +35,7 @@ def filter():
                                 sexes=SEXES,
                                 str_traits=STR_TRAITS)
 
-    filters : Filter_Options = {}
-    form = request.form
-
-    for trait in form.keys():
-
-        if trait == "id_val" and form[trait] != "":
-            id_option = form.get("id_option")
-            id_val = int(form.get("id_val"))
-            filters["id"] = (id_option, id_val)
-
-        if trait in STR_TRAITS and form[trait] != "":
-            filters[trait] = form.get(trait).capitalize()
-
-        if trait == "sex" and form[trait] != "":
-            filters[trait] = form.get("sex")
-
-        if trait == "date_val" and form[trait] != "":
-            date_option = form.get("date_option")
-            date_val = form.get("date_val")
-            filters["birth_date"] = (date_option, date_val)
-
-        if trait == "rating_val" and form[trait] != "":
-            rating_option = form.get("rating_option")
-            rating_val = float(form.get("rating_val"))
-            filters["rating"] = (rating_option, rating_val)
-
-        if (trait == "primary_skills" or trait == "secondary_skills") and form[trait] != "":
-            skills = form.get(trait).split(",")
-            filters[trait] = skills
+    filters = build_filter_option_from_form(request.form)
 
     session[FILTER_OPTIONS] = filters
 
--- helpers.py
+++ helpers.py
@@ -1,3 +1,6 @@
+from providers_types import Filter_Options
+from constants import STR_TRAITS
+
 def intersection(lst1: list, lst2: list):
     """ returns list that is only unique shared values between the input lists"""
     return list(set(lst1) & set(lst2))
@@ -7,4 +10,33 @@ def unique_union(lst1: list, lst2: list):
     return list(set(lst1 + lst2))
 
 def build_filter_option_from_form(form:dict):
-    return form
+    filters : Filter_Options = {}
+
+    for trait in form.keys():
+
+        if trait == "id_val" and form[trait] != "":
+            id_option = form.get("id_option")
+            id_val = int(form.get("id_val"))
+            filters["id"] = (id_option, id_val)
+
+        if trait in STR_TRAITS and form[trait] != "":
+            filters[trait] = form.get(trait).capitalize()
+
+        if trait == "sex" and form[trait] != "":
+            filters[trait] = form.get("sex")
+
+        if trait == "date_val" and form[trait] != "":
+            date_option = form.get("date_option")
+            date_val = form.get("date_val")
+            filters["birth_date"] = (date_option, date_val)
+
+        if trait == "rating_val" and form[trait] != "":
+            rating_option = form.get("rating_option")
+            rating_val = float(form.get("rating_val"))
+            filters["rating"] = (rating_option, rating_val)
+
+        if (trait == "primary_skills" or trait == "secondary_skills") and form[trait] != "":
+            skills = form.get(trait).split(",")
+            filters[trait] = skills
+
+    return filters
